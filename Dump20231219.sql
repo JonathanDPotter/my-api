ef343b97f10989fbc8c442af91135c1ae29fca93
Jonathan Potter <jonathan.d.potter@outlook.com>
2023-12-19 14:10:26 -0500

    Updated SQL file

--- Dump20231219.sql
+++ Dump20231219.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `dragonballz` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `dragonballz`;
 -- MySQL dump 10.13  Distrib 8.0.35, for Linux (x86_64)
 --
 -- Host: 127.0.0.1    Database: dragonballz
@@ -76,4 +78,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-12-19 13:51:41
+-- Dump completed on 2023-12-19 14:08:21
